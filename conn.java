01d2040c04becf2812ad7717e73783402d836ef6
Aditi Pawar <30751634+PawarAditi@users.noreply.github.com>
2020-09-09 12:25:47 +0530

    Add files via upload

---
+++ conn.java
@@ -0,0 +1,24 @@
+package fms;
+
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+class conn {
+    Connection con;
+    Statement st;
+
+    conn() {
+        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fees", "root", "root123");
+            st = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
